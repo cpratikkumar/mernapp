8572926059f2b86003293579aa90cb73cfa089a6
Pratik <iamcodery60@gmail.com>
2023-07-02 22:31:55 +0530

    update

--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const url =
+  process.env.URL ||
   "mongodb+srv://pratik123:pratik123@cluster0.rlnwhsm.mongodb.net/employee?retryWrites=true&w=majority";
+// connect
+
 mongoose
   .connect(url, {
     useNewUrlParser: true,
